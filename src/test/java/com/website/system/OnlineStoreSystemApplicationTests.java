107dc4fe65749f2f344071c847fa3c243c040987
Fredo <160517774+elfredoo@users.noreply.github.com>
2024-10-25 05:52:43 +0200

    initial commit
    first commit, creating repository

---
+++ src/test/java/com/website/system/OnlineStoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
