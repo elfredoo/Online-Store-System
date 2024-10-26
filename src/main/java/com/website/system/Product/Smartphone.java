9067e937a14e78f68202238df626b6d13d6530f1
Fredo <160517774+elfredoo@users.noreply.github.com>
2024-10-26 07:39:52 +0200

    added smartphone.java
    Added smartphone.java + accesssory.java

---
+++ src/main/java/com/website/system/Product/Smartphone.java
@@ -0,0 +1,48 @@
+package com.website.system.Product;
+
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+
+public class Smartphone extends Product{
+    private String color;
+    private int batteryCapacity;
+    @ManyToMany
+    @JoinTable(name = "smartphone_accessory")
+    private List<Accessory> accessories;
+
+    public Smartphone(Long id, String name,
+                      double price, int quantity,
+                      String color, int batteryCapacity,
+                      List<Accessory> accessories) {
+        super(id, name, price, quantity);
+        this.color = color;
+        this.batteryCapacity = batteryCapacity;
+        this.accessories = accessories;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public List<Accessory> getAccessories() {
+        return accessories;
+    }
+
+    public void setAccessories(List<Accessory> accessories) {
+        this.accessories = accessories;
+    }
+}
