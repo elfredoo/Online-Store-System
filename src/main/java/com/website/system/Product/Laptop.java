dec164b6a1953feeced7f77074de29c6bbe19d77
Fredo <160517774+elfredoo@users.noreply.github.com>
2024-10-26 07:21:55 +0200

    created laptop.java
    created laptop.java

---
+++ src/main/java/com/website/system/Product/Laptop.java
@@ -0,0 +1,24 @@
+package com.website.system.Product;
+
+public class Laptop extends Product{
+    private String processor;
+    private int ram;
+    private String graphicsCard;
+    private String diskType;
+    private int diskCapacity;
+    private double screenSize;
+
+    public Laptop(Long id, String name, 
+                  double price, int quantity,
+                  String processor, int ram,
+                  String graphicsCard, String diskType,
+                  int diskCapacity, double screenSize) {
+        super(id, name, price, quantity);
+        this.processor = processor;
+        this.ram = ram;
+        this.graphicsCard = graphicsCard;
+        this.diskType = diskType;
+        this.diskCapacity = diskCapacity;
+        this.screenSize = screenSize;
+    }
+}
