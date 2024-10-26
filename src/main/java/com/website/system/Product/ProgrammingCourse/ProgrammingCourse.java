04eb4f3c132437de7df1be31f01b05c928f03808
Fredo <160517774+elfredoo@users.noreply.github.com>
2024-10-26 12:53:04 +0200

    Changed relations in those classes
    Changed relation from @OneToOne to @OneToMany because there can be more than one CoursePreview assigned to one ProgrammingCourse

--- src/main/java/com/website/system/Product/ProgrammingCourse/ProgrammingCourse.java
+++ src/main/java/com/website/system/Product/ProgrammingCourse/ProgrammingCourse.java
@@ -2,16 +2,18 @@ package com.website.system.Product.ProgrammingCourse;
 
 import com.website.system.Product.Product;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
+import java.util.List;
+
 public class ProgrammingCourse extends Product {
-    @OneToOne
-    @JoinColumn(name = "course_preview_id")
-    private CoursePreview coursePreview;
+    @OneToMany
+    private List<CoursePreview> coursePreview;
 
     public ProgrammingCourse(Long id, String name,
                              double price, int quantity,
-                             CoursePreview coursePreview) {
+                             List<CoursePreview> coursePreview) {
         super(id, name, price, quantity);
         this.coursePreview = coursePreview;
     }
