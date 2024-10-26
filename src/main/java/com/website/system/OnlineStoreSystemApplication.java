981faf0206a57749f319d8855def5c81e9752783
Fredo <160517774+elfredoo@users.noreply.github.com>
2024-10-26 09:03:07 +0200

    created SportShoe.java and everything needed for it
    created SpottShoe.java, ShoelEngthToSizeConverter and NoSuchShoeSizeException

--- src/main/java/com/website/system/OnlineStoreSystemApplication.java
+++ src/main/java/com/website/system/OnlineStoreSystemApplication.java
@@ -1,13 +1,22 @@
 package com.website.system;
 
+import com.website.system.Product.SportShoe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class OnlineStoreSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OnlineStoreSystemApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(OnlineStoreSystemApplication.class, args);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj rozmiar stopy");
+        double shoeLength = Double.parseDouble(scanner.next());
+        SportShoe testShoe = new SportShoe(1L, "test shoe", 123.123, 12, shoeLength);
+        System.out.println(testShoe);
     }
 
 }
